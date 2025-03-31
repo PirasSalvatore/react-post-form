d5e65be4cae81106baec468cd5678855935c5447
PirasSalvatore <torep.92@gmail.com>
2025-03-31 15:46:55 +0200

    infinite loops

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,19 @@ function App() {
     <>
 
       <body>
+
         <section>
+
           <div className="container">
+
             <div className="card text-white bg-dark" >
+
               <div className="card-header">
                 <h1>Write you post</h1>
               </div>
               <div className="card-body">
-                <form className="row g-3 needs-validation" method='POST'>
+
+                <form className="row g-3 needs-validation">
 
                   <div className="col-md-4">
                     <label htmlFor="validationCustom01" className="form-label">First name</label>
@@ -26,26 +31,14 @@ function App() {
                     </div>
                   </div>
 
-                  <div className="col-md-4">
-                    <label htmlFor="validationCustom02" className="form-label">Last name</label>
-                    <input type="text" className="form-control" id="validationCustom02" value="Otto" required />
-                    <div className="valid-feedback">
-                      Looks good!
-                    </div>
-                  </div>
-
-                  <div className="col-md-4">
-                    <label htmlFor="validationCustom02" className="form-label">Last name</label>
-                    <input type="text-area" className="form-control" id="validationCustom02" value="Otto" required />
-                    <div className="valid-feedback">
-                      Looks good!
-                    </div>
-                  </div>
-
                   <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="public" />
                     <label className="form-check-label" htmlFor="public"> public? </label>
                   </div>
+
+                  <button class="btn btn-link">
+                    Submit
+                  </button>
 
 
                 </form>
